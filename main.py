54be0f1306a3ec17662593f9759140b35f309801
Melis Gökalp <melisgokalp@gmail.com>
2019-12-09 15:02:40 -0500

    preprocess class is made

---
+++ main.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from preprocess import Data_Processor
+from generator import Generator_Model
+from discriminator import Discriminator_Model
+import tensorflow_gan as tfgan
+import tensorflow_hub as hub
+import numpy as np
+
+batch_size = 30
+z_dim = 500
+
+# Train the model for one epoch.
+def train(generator, discriminator, real_images):
+    """
+    Train the model for one epoch. Save a checkpoint every 500 or so batches.
+
+    :param generator: generator model
+    :param discriminator: discriminator model
+    :param dataset_ierator: iterator over dataset, see preprocess.py for more information
+    :param manager: the manager that handles saving checkpoints by calling save()
+
+    :return: The average FID score over the epoch
+    """
+    # Loop over our data until we run out
+    #batch = getnextbatch(imgs, batch_id)
+    batch = real_images[0:100]
+    target_agegroup = None
+    for i in range (0, len(real_images), batch_size):
+    # for iteration, batch in enumerate(dataset_iterator):
+        # TODO: Train the model
+        batch = real_images[i:i+batch_size]
+        with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
+            g_output = generator(batch)
+            #fake img, real label
+            d_fake1_logit = discriminator(g_output, condition = true_label_64)
+            #real img, fake label
+            d_fake2_logit = discriminator(batch, condition = fake_label_64)
+            #real img, real label
+            d_real_logit = discriminator(batch, condition = true_label_64)
+
+
+
+            g_loss = generator.loss_function(batch, g_output, condition = true_label_64)
+            d_loss = discriminator.loss_function(d_real_logit, d_fake1_logit, d_fake2_logit)
+            
+        g_gradients = g_tape.gradient(g_loss,  generator.trainable_variables)
+        generator.optimizer.apply_gradients(zip(g_gradients, generator.trainable_variables))        
+        d_gradients = d_tape.gradient(d_loss,  discriminator.trainable_variables)
+        discriminator.optimizer.apply_gradients(zip(d_gradients, discriminator.trainable_variables))
+
+
+# Test the model by generating some samples.
+def test(generator):
+    """
+    Test the model.
+
+    :param generator: generator model
+
+    :return: None
+    """
+    # TODO: Replace 'None' with code to sample a batch of random images
+    img = tf.random.uniform([args.batch_size, args.z_dim], -1, 1)
+    img = generator(img)
+    
+
+    ### Below, we've already provided code to save these generated images to files on disk
+    # Rescale the image from (-1, 1) to (0, 255)
+    img = ((img / 2) - 0.5) * 255
+    # Convert to uint8
+    img = img.astype(np.uint8)
+    # Save images to disk
+    for i in range(0, args.batch_size):
+        img_i = img[i]
+        s = args.out_dir+'/'+str(i)+'.png'
+        imwrite(s, img_i)
+
+## --------------------------------------------------------------------------------------
+
+def main():
+    # Load a batch of images (to feed to the discriminator)
+    data_processor = Data_Processor()
+    celeb_metadata, image_metadata = Data_Processor.get_metadata()
+    real_images, real_labels_onehot, fake_labels_onehot, train_label_pairs, paths = Data_Processor.get_image(image_dir)
+    group_labels = image_metadata[1]
+    print(group_labels)
+    print("------------Preprocessing done.------------")
+    # Initialize generator and discriminator models
+    generator = Generator_Model()
+    discriminator = Discriminator_Model()
+
+    self.train(generator, discriminator, real_images)
+    try:
+        # Specify an invalid GPU device
+        with tf.device('/device:' + args.device):
+            if args.mode == 'train':
+                for epoch in range(0, args.num_epochs):
+                    print('========================== EPOCH %d  ==========================' % epoch)
+                    avg_fid = train(generator, discriminator, dataset_iterator, manager)
+                    print("Average FID for Epoch: " + str(avg_fid))
+                    # Save at the end of the epoch, too
+                    print("**** SAVING CHECKPOINT AT END OF EPOCH ****")
+                    manager.save()
+            if args.mode == 'test':
+                test(generator)
+    except RuntimeError as e:
+        print(e)
+
+if __name__ == '__main__':
+   main()
---
+++ preprocess.py
@@ -0,0 +1,93 @@
+import numpy as np
+import cv2
+import os  
+from scipy.io import loadmat
+from random import randint
+
+class data_processor:
+    def __init__(self, batch_size = 30, img_size= 128, shuffle=True, mode='train'):
+        self.metadata_dir = 'data/celebrity2000_meta.mat'
+        self.image_dir  = 'data/CACD2000' 
+        self.img_size  = img_size 
+        self.batch_size = batch_size
+        self.age_groups = [range(11, 21), range(21, 31), range(31, 41),range(41, 51), range(51, 151)]
+        self.pointer = 0
+
+    def get_metadata(self):
+        """
+        Gets the metadata from metadata directory and returns the metadata for both celebrities and images.
+
+            :param metadata_dir: directory of metadata
+
+            :return: two arrays of celebrity and image metadata
+        """ 
+        x = loadmat(self.metadata_dir) 
+        datatype = ['celebrityData', 'celebrityImageData']
+        # print(x)
+        names = x[datatype[0]][0][0][0]
+        identity = x[datatype[0]][0][0][1]
+        birth = x[datatype[0]][0][0][2]
+        rank = x[datatype[0]][0][0][3] #rank of the celebrity with same birth year in IMDB.com when the dataset was constructed
+        lfw = x[datatype[0]][0][0][4] #whether the celebrity is in LFW dataset 
+        celeb_metadata = [datatype, names, identity, birth, rank, lfw] #array of celeb features
+
+        image_age = x[datatype[1]][0][0][0]
+        image_id = x[datatype[1]][0][0][1]
+        image_year = x[datatype[1]][0][0][2]
+        image_features = x[datatype[1]][0][0][3]
+        image_filename = x[datatype[1]][0][0][4]
+
+        age_groups = self.age_groups
+        age_group_labels = [0 if (i in age_groups[0]) else 1 if (i in age_groups[1]) else 2 if (i in age_groups[2]) else 3 if (i in age_groups[3]) else 4 if (i in age_groups[4]) else None for i in image_age] 
+        image_metadata = [image_age, age_group_labels, image_id, image_year, image_features, image_filename] #array of image features
+
+        return celeb_metadata, image_metadata
+
+    def get_image(self):
+        """
+        Gets the image from  image path and returns the image.
+            Given an image data directory, this function opens and decodes the image stored in the directory.
+
+            :param image_dir: directory of image data
+
+            :return: arrays  of rgb images and paths
+        """ 
+        paths = os.listdir(self.image_dir)[0:1000]
+        imgs = np.ndarray([len(paths), self.img_size, self.img_size, 3])
+        for i in range(len(paths)):
+            img = cv2.imread(os.path.join(self.image_dir, paths[i]))
+            #uncomment below if  you want to display imgs
+            # if i == 0:
+            #     cv2.imshow('image', img)
+            #     cv2.waitKey(0)
+            #     cv2.destroyAllWindows() 
+            img = cv2.resize(img, (self.img_size, self.img_size))
+            img = img.astype(np.float32) 
+            imgs[i] = img 
+        celeb_metadata, image_metadata = self.get_metadata()
+        age_groups = image_metadata[1][0:len(paths)] 
+        train_label_pairs = self.get_fakelabels(age_groups)
+        fake_labels =  train_label_pairs[:,1]
+        real_labels_onehot = np.zeros((len(paths), self.img_size, self.img_size, 5))
+        real_labels_onehot[np.arange(len(paths)),:,:, age_groups] = np.ones((self.img_size,self.img_size)) 
+
+        fake_labels_onehot = np.zeros((len(paths), self.img_size, self.img_size, 5))
+        fake_labels_onehot[np.arange(len(paths)),:,:, fake_labels] = np.ones((self.img_size,self.img_size)) 
+        # stacked = np.concatenate((imgs, onehot), axis = 3) 
+        return imgs, real_labels_onehot, fake_labels_onehot, train_label_pairs, paths
+
+    def get_fakelabels(self, true_labels):
+        label_pairs = np.zeros((len(true_labels),2), dtype=int)
+        label_pairs[:,0] = true_labels
+        n = max(true_labels)
+        for i in range(len(true_labels)):
+            rand = randint(1,n)
+            true_label = true_labels[i]
+            fake_label = (true_label+rand)%n
+            label_pairs[i,1] = fake_label 
+        return label_pairs
+
+    def get_next_batch(self):
+        batch = None
+        return batch
+        
