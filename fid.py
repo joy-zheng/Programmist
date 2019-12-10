fce7fe322e603f08c27f714ea82d15b03ecd9052
joy-zheng <35876064+joy-zheng@users.noreply.github.com>
2019-12-10 02:32:56 -0500

    updated fid

--- fid.py
+++ fid.py
@@ -248,28 +248,3 @@ def load_images(path):
         assert im.dtype == final_images.dtype
         final_images[idx] = im
     return final_images
-
-
-if __name__ == "__main__":
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.add_option("--p1", "--path1", dest="path1", 
-                      help="Path to directory containing the real images")
-    parser.add_option("--p2", "--path2", dest="path2", 
-                      help="Path to directory containing the generated images")
-    parser.add_option("--multiprocessing", dest="use_multiprocessing",
-                      help="Toggle use of multiprocessing for image pre-processing. Defaults to use all cores",
-                      default=False,
-                      action="store_true")
-    parser.add_option("-b", "--batch-size", dest="batch_size",
-                      help="Set batch size to use for InceptionV3 network",
-                      type=int)
-    
-    options, _ = parser.parse_args()
-    assert options.path1 is not None, "--path1 is an required option"
-    assert options.path2 is not None, "--path2 is an required option"
-    assert options.batch_size is not None, "--batch_size is an required option"
-    images1 = load_images(options.path1)
-    images2 = load_images(options.path2)
-    fid_value = calculate_fid(images1, images2, options.use_multiprocessing, options.batch_size)
-    print(fid_value)
