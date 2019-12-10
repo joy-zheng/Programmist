165c184a81286303131c1ec922819b9b11faab40
Melis Gökalp <melisgokalp@gmail.com>
2019-12-10 00:34:35 -0500

    fix fid

--- eval/fid_score.py
+++ eval/fid_score.py
@@ -48,7 +48,7 @@ except ImportError:
     # If not tqdm is not available, provide a mock version of it
     def tqdm(x): return x
 
-from inception import InceptionV3
+from .inception import InceptionV3 
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('path', type=str, nargs=2,
